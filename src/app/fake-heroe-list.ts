81bfe551c9b4f0769cd61395d3b5db6b0936e26b
Emmanuel Castro Martel <emmanuelcastromartel@outlook.es>
2023-05-15 15:55:23 +0100

    Implemented the styles of the Heroes component and created a fake list of heroes for the application to display

---
+++ src/app/fake-heroe-list.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero.interface';
+
+export const listOfHeroes: Hero[] = [
+  { id: 0, name: 'Iron Man' },
+  { id: 1, name: 'Hulk' },
+  { id: 2, name: 'Thor' },
+  { id: 3, name: 'Capitán América' },
+  { id: 4, name: 'Viuda Negra' },
+  { id: 5, name: 'Ojo de Halcón' },
+  { id: 6, name: 'Bruja Escarlata' },
+  { id: 7, name: 'Spiderman' },
+  { id: 8, name: 'Visión' },
+  { id: 9, name: 'Capitana Marvel' },
+  { id: 10, name: 'Doctor Extraño' }
+];
